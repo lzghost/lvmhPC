4e9ceefbe19a679e65d7eb8240c987d38f3797b1
lzghost <529881171@qq.com>
2018-06-25 03:01:00 +0800

    修改登录

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,33 +1,29 @@
 <template>
-  <div class="container">
+  <div class="container login-wrapper">
     <div class="logo"></div>
     <div class="words">Internal purchase</div>
     <!--登录部分 start-->
-    <el-form :model="loginForm" ref="loginForm" label-width="100px" class="loginFormStyle">
-      <el-form-item prop="userName">
-        <el-input
-          v-model="loginForm.userName"
-          auto-complete="off">
-          <i slot="prefix">
-            <img src="../../assets/icon/user.png" alt="">
-          </i>
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="passWord">
-        <el-input
-          type="password"
-          v-model="loginForm.passWord"
-          auto-complete="off">
-          <i slot="prefix">
-            <img src="../../assets/icon/password.png" alt="">
-          </i>
-        </el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button>提交</el-button>
-      </el-form-item>
-    </el-form>
-    <!--登录部分 end-->
+    <el-col :span="6" :offset="9">
+      <el-form :model="loginForm" ref="loginForm" class="loginFormStyle">
+        <el-form-item prop="userName">
+          <el-input class="input-login"
+                    v-model="loginForm.userName"
+                    auto-complete="off">
+            <img slot="prepend" src="../../assets/icon/user.png" style="background-color: transparent">
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="passWord" size="large">
+          <el-input type="password" clearable resize="both" class="input-login" v-model="loginForm.passWord" placeholder="请输入密码" >
+            <img slot="prepend" src="../../assets/icon/password.png" alt="">
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="loginBtn">登录</el-button>
+        </el-form-item>
+      </el-form>
+      <!--登录部分 end-->
+    </el-col>
+
   </div>
 </template>
 
@@ -45,10 +41,7 @@
     mounted() {
 
     },
-    mounted(){
-
-    },
-    method(){
+    methods:{
 
     }
   }
@@ -59,23 +52,23 @@
   .container{
     width:100%;
     height:100%;
-    background:url('../../assets/banner-Trail.png') no-repeat center;
-    background-size:100% auto;
+    background:url('../../assets/pc-bg.png') no-repeat center;
+    background-size:100% 100%;
+    font-size: 100px;
   }
   .container:before{
     content: "";
     display: table;
   }
-  .logo{
+  .container .logo{
     width:339px;
     height:60px;
     margin:auto;
-    margin-top:185px;
+    margin-top: 10%;
     background-image:url('../../assets/logo.png');
     background-size:100% 100%;
   }
   .words{
-    margin:auto;
     margin-top:49px;
     margin-bottom:68px;
     font-size:36px;
@@ -83,10 +76,26 @@
     color:#fff;
   }
   .loginFormStyle{
-    width:400px;
     margin:auto;
   }
-  .loginFormStyle .el-input .el-input__inner{
-    height:52px !important;
+
+  .input-login{
+    height: 52px;
+    border-radius: 8px;
+    border: 1px solid rgba(224, 227, 229, 1);
+    background-color: transparent;
+    color: white;
+  }
+  .el-input__inner{
+    background-color: transparent;
+    color: white;
+  }
+  .loginBtn{
+    height: 52px;
+    width: 100%;
+    border-radius: 8px;
+    border: 1px solid rgba(224, 227, 229, 1);
+    background-color: transparent;
+    color: white;
   }
 </style>
