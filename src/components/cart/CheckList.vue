54a200cdd086ba3d4047a05f951c94830abdd058
FFelix <13314209626@163.com>
2018-07-20 09:18:46 +0800

    移动端购物车功能简单修改

--- src/components/cart/CheckList.vue
+++ src/components/cart/CheckList.vue
@@ -1,27 +1,36 @@
 <template>
   <div class="checklist-mb-wrapper">
     <el-row
-    v-for="(one, index) in currentOptions"
-    v-bind:key="index"
-    class="list"
+      v-for="(one, index) in options"
+      v-bind:key="index"
+      class="list"
     >
       <div>
         <el-col :span="3">
-        <check-icon :value.sync="demo1" style="margin-top:20px;"></check-icon>
+          <check-icon
+            @click.native="toggleCheck(index)"
+            :value.sync="checkList[index]"
+            style="margin-top:20px;"
+          ></check-icon>
         </el-col>
         <el-col :span="6">
-          <img src="" width="70" height="70">
+          <img :src="one.pic" width="70" height="70">
         </el-col>
         <el-col :span="14" style="margin-left:5px;">
-          <div class="good-name"> 风中玫瑰（ROSE DES VENTS）香水 </div>
-          <div class="good-norm"> 50ml；01 </div>
+          <div class="good-name"> {{one.name}} </div>
+          <div class="good-norm"> {{one.spec1}}；{{one.spec2}} </div>
           <el-row>
-          <el-col :span="9">
-            <div class="good-price"> ¥300 </div>
-          </el-col>
-          <el-col :span="15">
-            <el-input-number size="mini"></el-input-number>
-          </el-col>
+            <el-col :span="9">
+              <div class="good-price"> ¥{{one.price}} </div>
+            </el-col>
+            <el-col :span="15">
+              <el-input-number
+                size="mini"
+                :min="1"
+                v-model="one.qty"
+                @change="changeCount(one)"
+              ></el-input-number>
+            </el-col>
           </el-row>
         </el-col>
       </div>
@@ -30,61 +39,89 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex"
-import { CheckIcon } from 'vux'
+  import { mapState, mapMutations } from 'vuex'
+  import { CheckIcon } from 'vux'
+  import { orderPlace, updateCart, getCartNum } from '@/service/index'
 
-export default {
-  props: {
-    options: {
-      type: Array,
-      required: true
+  export default {
+    props: {
+      options: {
+        type: Array,
+        required: true
+      },
     },
-  },
-  data() {
-    return {
-      demo1: true,
-      currentOptions: this.options,
-    };
-  },
-  async beforeMount() {},
-  created() {
-    this.currentOptions = this.options;
-  },
-  mounted() {},
-  components: {
-    CheckIcon,
-  },
-  computed: {},
-  methods: {},
-  watch: {}
-};
+    data () {
+      return {
+        demo: true,
+        checkList: []
+      }
+    },
+    created () {
+      for (let i = 0; i < this.options.length; i++) {
+        this.checkList.push(true)
+      }
+    },
+    components: {
+      CheckIcon,
+    },
+    computed: {
+      ...mapState([
+        'global', 'categories', 'campaign', 'bread', 'goods'
+      ])
+    },
+    methods: {
+      ...mapMutations({
+        initCartNum: 'INIT_CART_NUM',
+      }),
+      async changeCount (goods) {
+        const param = {}
+        param.items = []
+        param.items.push({
+          qty: goods.qty,
+          productId: goods.productId
+        })
+        const res = await updateCart(this.campaign.id, param)
+        if (res.status === 0) {
+          const cartRes = await getCartNum(this.campaign.id)
+          this.initCartNum({cartNum: cartRes.data})
+        }
+      },
+      toggleCheck (index) {
+        console.log('index = ' + index)
+        this.$set(this.checkList, index, !this.checkList[index])
+      }
+    },
+    watch: {}
+  }
 </script>
 
 <style scoped>
-.checklist-mb-wrapper{
-  width: 100%;
-}
-.list{
-  padding: 18px 10px;
-}
-.good-name {
-  line-height: 17px;
-  color: rgba(51, 51, 51, 1);
-  font-size: 12px;
-  text-align: left;
-}
+  .checklist-mb-wrapper {
+    width: 100%;
+  }
 
-.good-norm {
-  line-height: 17px;
-  color: rgba(153, 153, 153, 1);
-  font-size: 12px;
-  text-align: left;
-}
+  .list {
+    padding: 18px 10px;
+  }
 
-.good-price {
-  line-height: 20px;
-  color: rgba(185, 142, 43, 1);
-  font-size: 14px;
-  text-align: left;
-}
+  .good-name {
+    line-height: 17px;
+    color: rgba(51, 51, 51, 1);
+    font-size: 12px;
+    text-align: left;
+  }
+
+  .good-norm {
+    line-height: 17px;
+    color: rgba(153, 153, 153, 1);
+    font-size: 12px;
+    text-align: left;
+  }
+
+  .good-price {
+    line-height: 20px;
+    color: rgba(185, 142, 43, 1);
+    font-size: 14px;
+    text-align: left;
+  }
 </style>
