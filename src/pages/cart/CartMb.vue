d32decafeb32c98537e475d873fd477c00284942
lzghost <529881171@qq.com>
2018-07-20 10:31:42 +0800

    增加移动端代码

--- src/pages/cart/CartMb.vue
+++ src/pages/cart/CartMb.vue
@@ -1,9 +1,51 @@
 <template>
   <div>
-    <check-list-mb :options="cartInfoList" style="margin-bottom:115px;"></check-list-mb>
+    <!--<check-list-mb-->
+      <!--:options="cartInfoList"-->
+      <!--style="margin-bottom:115px;"-->
+    <!--&gt;</check-list-mb>-->
+    <div class="checklist-mb-wrapper">
+      <el-row
+        v-for="(one, index) in cartInfoList"
+        v-bind:key="index"
+        class="list"
+      >
+        <div>
+          <el-col :span="3">
+            <check-icon
+              :value.sync="one.checked"
+              style="margin-top:20px;"
+            ></check-icon>
+          </el-col>
+          <el-col :span="6">
+            <img :src="one.pic" width="70" height="70">
+          </el-col>
+          <el-col :span="14" style="margin-left:5px;">
+            <div class="good-name"> {{one.name}}</div>
+            <div class="good-norm"> {{one.spec1}}；{{one.spec2}}</div>
+            <el-row>
+              <el-col :span="9">
+                <div class="good-price"> ¥{{one.price}}</div>
+              </el-col>
+              <el-col :span="15">
+                <el-input-number
+                  size="mini"
+                  :min="1"
+                  v-model="one.qty"
+                  @change="changeCount(one)"
+                ></el-input-number>
+              </el-col>
+            </el-row>
+          </el-col>
+        </div>
+      </el-row>
+    </div>
     <el-row class="nav-good">
       <el-col :span="7" style="height:100%">
-        <check-icon :value.sync="demo1" style="margin-top:13px;">全选</check-icon>
+        <check-icon
+          :value.sync="demo1"
+          style="margin-top:13px;"
+        >全选</check-icon>
       </el-col>
       <el-col :span="7" style="height:100%">
         <div class="total">
@@ -18,16 +60,16 @@
 </template>
 
 <script>
-  import { mapState, mapMutations } from 'vuex'
-  import { CheckIcon } from 'vux'
-  import { cartInfo } from '@/service/index'
-  import CheckListMb from '@/components/cart/CheckList.vue'
+  import {mapState, mapMutations} from 'vuex'
+  import {CheckIcon} from 'vux'
+  import {cartInfo, orderPlace, updateCart, getCartNum} from '@/service/index'
+
 
   export default {
-    data () {
+    data() {
       return {
         temp: [],
-        demo1: true,
+        demo1: false,
         total: 0,
         cartInfoList: []
       }
@@ -35,24 +77,34 @@
     computed: {
       ...mapState([
         'global', 'categories', 'campaign', 'bread', 'goods'
-      ])
+      ]),
     },
-    mounted () {
+    mounted() {
       this.getCartInfo()
     },
     components: {
-      CheckListMb,
       CheckIcon,
     },
     methods: {
-      async getCartInfo () {
+      async getCartInfo() {
         const res = await cartInfo(this.campaign.id)
         if (res.status === 0) {
+          res.data.map(item => item.checked = false)
           this.cartInfoList = res.data
         }
-      }
+      },
     },
-    watch: {}
+    watch: {
+      cartInfoList: {
+        deep: true,
+        handler: function(val, oldVal){
+          console.log(val);
+        }
+      },
+      demo1() {
+        console.log(2)
+      }
+    }
   }
 </script>
 
@@ -60,7 +112,7 @@
   .nav-good {
     height: 49px;
     background-color: rgba(255, 255, 255, 1);
-    box-shadow: undefined, inset 0px 0.5px 0px 0px rgba(0, 0, 0, 0.25);
+    box-shadow: inset 0px 0.5px 0px 0px rgba(0, 0, 0, 0.25);
     position: fixed;
     background-image: -webkit-gradient(linear, left top, left bottom, from(#d9d9d9), color-stop(50%, #d9d9d9), color-stop(50%, transparent));
     background-image: linear-gradient(180deg, #d9d9d9, #d9d9d9 50%, transparent 50%);
@@ -95,4 +147,32 @@
     height: 100%;
   }
 
+  .checklist-mb-wrapper {
+    width: 100%;
+  }
+
+  .list {
+    padding: 18px 10px;
+  }
+
+  .good-name {
+    line-height: 17px;
+    color: rgba(51, 51, 51, 1);
+    font-size: 12px;
+    text-align: left;
+  }
+
+  .good-norm {
+    line-height: 17px;
+    color: rgba(153, 153, 153, 1);
+    font-size: 12px;
+    text-align: left;
+  }
+
+  .good-price {
+    line-height: 20px;
+    color: rgba(185, 142, 43, 1);
+    font-size: 14px;
+    text-align: left;
+  }
 </style>
